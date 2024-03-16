f875568f7e8f4ee1f16960939914c16d7e9b41b0
Leerv474 <roman.lee474@gmail.com>
2024-03-16 23:58:15 +0300

    document manipulations done

--- Project/src/Main.java
+++ Project/src/Main.java
@@ -1,7 +1,24 @@
 import TextDocument.Document;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         Document test = new Document("data/test.txt");
+        test.setAuthor("Lee");
+        test.setTitle("Testing");
+
+        List<String> newContents = new ArrayList<>();
+        newContents.add("Hello");
+        newContents.add("Hello");
+        newContents.add("Hello");
+        newContents.add("Hello");
+        newContents.add("Hello");
+
+        test.setFileContents(newContents);
+
+        test.saveDocument();
+
     }
 }
--- Project/src/TextDocument/Document.java
+++ Project/src/TextDocument/Document.java
@@ -6,7 +6,6 @@ import java.io.File;
 import java.io.FileWriter;
 
 import java.nio.file.Paths;
-import java.text.SimpleDateFormat;
 import java.util.List;
 import java.util.stream.Stream;
 import java.time.LocalDate;
@@ -15,23 +14,22 @@ import java.time.format.DateTimeFormatter;
 public class Document {
     private String title;
     private String author;
-    private String path;
-    private long fileSize; // fileSize determined by File in bytes
+    private final String path;
     private LocalDate creationDate;
     private List<String> fileContents;
-    private boolean fileExists;
 
-    // I need this class to be able to create new one if it doesn't exist, open existing, write
 
     private void parseData() {
         List<String> lineList;
         try (Stream<String> lines = Files.lines(Paths.get(path))) {
             lineList = lines.toList();
         } catch (IOException e) {
-            this.fileExists = false;
+            System.err.println("Failed to read file");
             return;
         }
-        this.fileExists = true;
+        if (lineList.size() < 3){
+            return;
+        }
 
         boolean hasTitle = lineList.get(0).contains("Title:");
         boolean hasAuthor = lineList.get(1).contains("Author:");
@@ -56,35 +54,107 @@ public class Document {
         parseData();
     }
 
-    public void setTitle(String title) {
+    /**
+     * @return An integer value indicating the result:
+     *      <ul>
+     *          <li> 0 - success</li>
+     *          <li> 1 - failure </li>
+     *      </ul>
+     */
+    public int setTitle(String title) {
         if (title.isEmpty()) {
-            System.err.println("Title wasn't given");
-            return;
+            return 1;
         }
         this.title = title;
+        return 0;
     }
-    public void setAuthor(String author) {
+    /**
+     * @return An integer value indicating the result:
+     *      <ul>
+     *          <li> 0 - success</li>
+     *          <li> 1 - failure </li>
+     *      </ul>
+     */
+    public int setAuthor(String author) {
         if (author.length() < 2) {
-            System.err.println("Author name if too short");
-            return;
+            return 1;
         }
         this.author = author;
+        return 0;
     }
-    public void setFileContents(List<String> fileContents) {
+
+    /**
+     * @return An integer value indicating the result:
+     *      <ul>
+     *          <li> 0 - success</li>
+     *          <li> 1 - failure </li>
+     *      </ul>
+     */
+    public int setFileContents(List<String> fileContents) {
+        if (fileContents.isEmpty()) {
+            return 1;
+        }
         this.fileContents = fileContents;
+        return 0;
     }
-    public void saveDocument() {
+
+    public String getTitle() {
+        return this.title;
+    }
+    public String getAuthor() {
+        return this.author;
+    }
+    public List<String> getFileContents() {
+        return this.fileContents;
+    }
+    public long getFileSize() {
+        File file = new File(path);
+        return file.length();
+    }
+
+
+    /**
+     * This method creates a file and writes data to it.
+     *
+     * @return An integer value indicating the result:
+     *         <ul>
+     *         <li>0 - success</li>
+     *         <li>1 - failed creating a file</li>
+     *         <li>2 - title wasn't specified</li>
+     *         <li>3 - author wasn't specified</li>
+     *         <li>4 - failed writing to a file</li>
+     *         </ul>
+     */
+    public int saveDocument() {
+        File file = new File(path);
+        if (!file.exists()) {
+            try {
+                if (!file.createNewFile()) {
+                    return 1;
+                }
+            } catch (IOException e) {
+                return 1;
+            }
+        }
+        if (this.title.isEmpty()) {
+            return 2;
+        }
+        if (this.author.isEmpty()) {
+            return 3;
+        }
         try (FileWriter writer = new FileWriter(path)){
             writer.write("Title: " + this.title + '\n');
             writer.write("Author: " + this.author + '\n');
-            SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
-            String dateString = dateFormat.format(this.creationDate);
+            this.creationDate = LocalDate.now();
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+            String dateString = this.creationDate.format(formatter);
             writer.write("Creation Date: " + dateString + "\n\n");
             for (String line : fileContents) {
                 writer.write(line + '\n');
             }
         } catch (IOException e) {
-            System.err.println("Error writing to file: " + e.getMessage());
+            return 4;
         }
+        return 0;
     }
 }
